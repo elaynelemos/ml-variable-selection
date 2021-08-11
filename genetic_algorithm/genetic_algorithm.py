f03293c4f0faa43ff055470432c54db266c14d35
elaynelemos <elayne.l.lemos@gmail.com>
2021-08-10 22:52:24 -0300

    feat: improve fit

--- genetic_algorithm/genetic_algorithm.py
+++ genetic_algorithm/genetic_algorithm.py
@@ -5,7 +5,7 @@ from genetic_algorithm.models.population import Population
 def genetic_algorithm(
             population_size:int,
             max_generations: int,
-            mutation_rate:float,
+            mutation_rate: float,
             database: DataFrame):
 
     population = Population(population_size, mutation_rate, database)
@@ -17,38 +17,27 @@ def genetic_algorithm(
 
         population.reproduction()
 
-    generations.append(population)
-    print_generation(len(generations), population)
     solution = generations[-1].best_fit().value()
-    print(f'Solution: f({solution}) = {generations[-1].best_fit().fit}')
+    print(f"\n\nChromossome '{solution}' => {generations[-1].best_fit().fit}")
 
     return solution
 
 
 def kept_fit(generations):
-    average_fits = []
     best_fits = []
-    target_error = 1e-4
+    target_error = 1e-8
     min_generations = 5
 
     if len(generations) < min_generations:
         return False
 
     for generation in generations:
-        average_fits.append(generation.average_fit())
-        best_fits.append(generation.best_fit())
+        best_fits.append(generation.best_fit().fit)
 
     init = len(generations) - min_generations + 1
-    average = average_fits[init]
     best = best_fits[init]
-    for i in range(init + 1, len(average_fits)):
-        if abs(average_fits[i] - average) > target_error:
-            average = None
-
+    for i in range(init + 1, len(best_fits)):
         if abs(best_fits[i] - best) > target_error:
-            best = None
-
-        if average is None and best is None:
             return False
 
     return True
@@ -59,9 +48,7 @@ def print_generation(gen_index, generation):
     chromossomes = generation.chromossomes
 
     for i in range(len(chromossomes)):
-        chromossomes_genes.append(
-            f"Chromossome {i + 1} '{chromossomes[i].genes}' = "
-            f'{chromossomes[i].value()}')
+        chromossomes_genes.append(f"Chromossome {i + 1} '{chromossomes[i].genes}'")
 
     output = '\n'.join(chromossomes_genes)
     print(f'Generation #{gen_index}')
